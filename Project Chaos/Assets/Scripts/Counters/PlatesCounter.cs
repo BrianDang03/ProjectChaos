86442fc2d892037cd7d92a403e0131b2e597b0a1
Brian Dang <pandabrian1@gmail.com>
2023-12-28 12:04:01 -0700

    PlatesCounter
    Added a Plates Counter that would periodically spawns plates for the player to pick up and put food on.

---
+++ Project Chaos/Assets/Scripts/Counters/PlatesCounter.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatesCounter : BaseCounter
+{
+    public event EventHandler OnPlateSpawned;
+    public event EventHandler OnPlateRemoved;
+
+    [SerializeField] private KitchenObjectSO plateKitchenObjectSO;
+    [SerializeField] float spawnPlateTimerMax = 4f;
+    [SerializeField] private int platesSpawnedAmountMax = 4;
+
+    private int platesSpawnedAmount;
+    private float spawnPlateTimer;
+
+    private void Update()
+    {
+        spawnPlateTimer += Time.deltaTime;
+        if (spawnPlateTimer > spawnPlateTimerMax)
+        {
+            spawnPlateTimer = 0f;
+
+            if (platesSpawnedAmount < platesSpawnedAmountMax)
+            {
+                platesSpawnedAmount++;
+                OnPlateSpawned?.Invoke(this, EventArgs.Empty);
+            }
+        }
+    }
+
+    public override void Interact(Player player)
+    {
+        if (!player.HasKitchenObject())
+        {
+            //If Player is Empty Handed
+            if (platesSpawnedAmount > 0)
+            {
+                //If there there is at least one plate
+                platesSpawnedAmount--;
+
+                OnPlateRemoved?.Invoke(this, EventArgs.Empty);
+
+                KitchenObject.SpawnKitchenObject(plateKitchenObjectSO, player);
+            }
+        }
+    }
+}
